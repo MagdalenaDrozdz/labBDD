7f88585d86d3747692f7744f655b33ffbbcce08d
Dawid <215920@edu.p.lodz.pl>
2020-06-06 22:00:13 +0200

    feature1_change

--- src/main/java/edu/iis/mto/bdd/trains/services/InterinaryService.java
+++ src/main/java/edu/iis/mto/bdd/trains/services/InterinaryService.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 import java.util.List;
 
 public interface InterinaryService {
-    int EXPECTED_TIME_ARRIVAL = 15;
+    int EXPECTED_TIME_ARRIVAL = 30;
 
     List<LocalTime> findNextDepartures(String departure, String destination, LocalTime startTime);
 }
--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -5,6 +5,7 @@ import java.util.List;
 import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import edu.iis.mto.bdd.trains.services.InterinaryService;
 import edu.iis.mto.bdd.trains.services.InterinaryServiceImpl;
+import edu.iis.mto.bdd.trains.services.TimetableService;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -23,7 +24,8 @@ public class OptimalItinerarySteps {
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-        interinaryService = new InterinaryServiceImpl(new InMemoryTimetableService());
+        TimetableService timetableService = new InMemoryTimetableService();
+        interinaryService = new InterinaryServiceImpl(timetableService);
     }
 
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
